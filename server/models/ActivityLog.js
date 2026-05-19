a6c5046c71e83131085126110d1d6c75b1219670
MorePrajita <moreprajita2606@gmail.com>
2026-05-19 19:01:17 +0530

    Clean up ActivityLog.js by removing commented code
    Removed commented-out code for activity log and schema definitions.

--- server/models/ActivityLog.js
+++ server/models/ActivityLog.js
@@ -1,25 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const activityLogSchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//   name: { type: String, required: true },
-//   duration: { type: Number, required: true },
-//   caloriesBurned: { type: Number, required: true },
-//   date: { type: String, required: true } // YYYY-MM-DD
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("ActivityLog", activityLogSchema);
-// const mongoose = require("mongoose");
-
-// const activitySchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-//   exerciseType: { type: String, required: true }, // e.g., Running, Cycling
-//   duration: { type: Number, required: true },    // in minutes
-//   caloriesBurned: { type: Number, required: true },
-//   date: { type: Date, default: Date.now }
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("Activity", activitySchema);
 const mongoose = require("mongoose");
 
 const activitySchema = new mongoose.Schema({
